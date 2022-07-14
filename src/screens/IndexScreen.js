d9d989c8f3b230dad4cc04e63431d697ca039f1e
Mike R <or4ngedeuce@gmail.com>
2022-07-14 08:57:10 -0500

    Setup new state with context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, StyleSheet, Text, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  //const value = useContext(BlogContext);
+
+  const { data, addBlogPost } = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={() => addBlogPost()}></Button>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
